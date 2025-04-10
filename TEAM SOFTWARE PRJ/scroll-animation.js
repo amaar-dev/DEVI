09c63651b603cd7bf009db8635379098e9c1f7df
Muhammad Ammar <muh.ammar02@gmail.com>
2025-04-10 16:56:48 +0500

    Add files via upload

---
+++ TEAM SOFTWARE PRJ/scroll-animation.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollElements = document.querySelectorAll("[data-scroll]");
+
+    const elementInView = (el, offset = 100) => {
+        const elementTop = el.getBoundingClientRect().top;
+        return (
+            elementTop <=
+            (window.innerHeight || document.documentElement.clientHeight) - offset
+        );
+    };
+
+    const displayScrollElement = (element) => {
+        element.classList.add("visible");
+    };
+
+    const hideScrollElement = (element) => {
+        element.classList.remove("visible");
+    };
+
+    const handleScrollAnimation = () => {
+        scrollElements.forEach((el) => {
+            if (elementInView(el, 150)) {
+                displayScrollElement(el);
+            } else {
+                hideScrollElement(el);
+            }
+        });
+    };
+
+    window.addEventListener("scroll", () => {
+        handleScrollAnimation();
+    });
+
+    // Initial check
+    handleScrollAnimation();
+});
